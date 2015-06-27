b180ee37d41cb09e516837a784ce5404cd9f65ae
samo@stellarbluetechnologies.com <samo@stellarbluetechnologies.com>
2015-06-26 21:02:14 -0500

    Added comments for readability

--- zombit-infection/solution.java
+++ zombit-infection/solution.java
@@ -3,6 +3,11 @@
  *  accessor for population, requiring population[y][x] rather than population[x][y]
  *
  *  Solution on this page is pretty much guaranteed to be inefficient, but it works.
+ *
+ *
+ *  These answers are intended to be an open topic for discussion and not to be used
+ *  to complete the challenges itself. If you copy my code to use within Foobar, you
+ *  are a cheater! :)
  */
 public static int[][] answer(int[][] population, int x, int y, int strength) { 
 
@@ -12,18 +17,27 @@ public static int[][] answer(int[][] population, int x, int y, int strength) {
             return population;
             
         population[y][x] = -1;
+
+        // Main iteration loop
         while (true) {
+            // Deep copy as Java passes by reference and will result in incorrect answers if we don't do this.
         	int[][] population_pass = array_deep_copy(population);
+
+            // Run an infection cycle over the population array as it is currently
         	int[][] infection_cycle = run_infection_pass(population, strength);
+
+            // If our array is unchanged from the previous iteration we are done.
             if (arrays_equal(population_pass, infection_cycle))
                 return infection_cycle;
             
+            // Assign population to the new infection cycle
             population = infection_cycle;
         }
     }
     
     private static boolean arrays_equal(int[][] base, int[][] comparison) {
         // We're never going to have arrays of non-equal dimensions
+        // Just a basic comparison between two 2D arrays provided they are equal dimensions
         for (int i = 0; i < base.length; i++) {
             for (int j = 0; j < base[i].length; j++) {
                 if (base[i][j] != comparison[i][j])
@@ -33,7 +47,20 @@ public static int[][] answer(int[][] population, int x, int y, int strength) {
         return true;
     }
     
+    private static int[][] array_deep_copy(int[][] original) {
+        // Simple deep copy function for 2D arrays
+        int[][] holder = new int[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            holder[i] = new int[original[i].length];
+            for (int j = 0; j < original[i].length; j++) {
+                holder[i][j] = original[i][j];
+            }
+        }
+        return holder;
+    }
+    
     private static int[][] run_infection_pass(int[][] population, int strength) {
+        // Main infection pass loop. Broken up for easier reading.
         for (int col = 0; col < population.length; col++) {
             for (int row = 0; row < population[col].length; row++) {
                 if (population[col][row] == -1) {
@@ -43,6 +70,7 @@ public static int[][] answer(int[][] population, int x, int y, int strength) {
                     int previous_row = row - 1;
                     int next_row = row + 1;
                     
+                    // Verify all cases for index out of bounds exceptions
                     if (previous_col >= 0) {
                         if (population[previous_col][row] <= strength)
                             population[previous_col][row] = -1;
@@ -63,15 +91,4 @@ public static int[][] answer(int[][] population, int x, int y, int strength) {
             }
         }
         return population;
-    }
-    
-    private static int[][] array_deep_copy(int[][] original) {
-    	int[][] holder = new int[original.length][];
-    	for (int i = 0; i < original.length; i++) {
-    		holder[i] = new int[original[i].length];
-    		for (int j = 0; j < original[i].length; j++) {
-    			holder[i][j] = original[i][j];
-    		}
-    	}
-    	return holder;
     }
